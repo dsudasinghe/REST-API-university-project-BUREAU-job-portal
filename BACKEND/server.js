e69f968c401d5a38f7098e8d69982e2c9a3e25db
Dananjaya Sudasinghe <dananjayasudasinghe@gmail.com>
2022-04-30 08:43:15 +0530

    upddated

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
+require("dotenv").config();
 
 const app = express();
 
@@ -11,3 +12,16 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Mongodb Connection success!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Running on PORT ${PORT}`);
+});
