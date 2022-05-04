b8f9178bee3987d394461ace5b4f503e6bdfc6b6
Dananjaya Sudasinghe <dananjayasudasinghe@gmail.com>
2022-05-04 10:36:44 +0530

    feedback routes complete

--- BACKEND/controller/feedbackController.js
+++ BACKEND/controller/feedbackController.js
@@ -17,6 +17,58 @@ const feedbackController = {
       return res.status(500).json({ msg: err.message });
     }
   },
+
+  sendReply: async (req, res) => {
+    try {
+      const feedback = await Feedbacks.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          reply: req.body.reply,
+        }
+      );
+      if (!feedback) return res.status(400).json({ msg: "Action failed!" });
+      res.json({ msg: "Reply sent!" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+
+  getFeedbacks: async (req, res) => {
+    try {
+      const feedbacks = await Feedbacks.find();
+      res.json({ feedbacks });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+
+  getFeedbackById: async (req, res) => {
+    try {
+      const feedback = await Feedbacks.findById(req.params.id);
+      res.json({ feedback });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+
+  getFeedbackByUid: async (req, res) => {
+    try {
+      const feedbacks = await Feedbacks.find({ userId: req.params.userId });
+      res.json({ feedbacks });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+
+  deleteFeedback: async (req, res) => {
+    try {
+      const feedback = await Feedbacks.findByIdAndDelete(req.params.id);
+      if (!feedback) return res.status(400).json({ msg: "Invalid operation" });
+      res.json({ msg: "Delete succussful!" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
 };
 
 module.exports = feedbackController;
--- BACKEND/routes/feedbackRoutes.js
+++ BACKEND/routes/feedbackRoutes.js
@@ -3,4 +3,14 @@ const feedbackController = require("../controller/feedbackController");
 
 router.post("/add", feedbackController.addFeedback);
 
+router.put("/reply/:id", feedbackController.sendReply);
+
+router.get("/", feedbackController.getFeedbacks);
+
+router.get("/:id", feedbackController.getFeedbackById);
+
+router.get("/user_feedbacks/:userId", feedbackController.getFeedbackByUid);
+
+router.delete("/delete/:id", feedbackController.deleteFeedback);
+
 module.exports = router;
